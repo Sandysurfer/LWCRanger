e0c92b891b4ef302ec0c2e152430de71a422b63b
Sandysurfer <115895789+Sandysurfer@users.noreply.github.com>
2023-10-20 18:14:53 +0530

    m

--- force-app/main/default/lwc/callApex/callApex.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement, wire } from 'lwc';
-import methodTest from '@salesforce/apex/LightningDataTableController.methodTest';
-export default class CallApex extends LightningElement {
-    @wire(methodTest) apexProperty;
-}
--- force-app/main/default/lwc/mycomonentjune23/mycomonentjune23.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class Mycomonentjune23 extends LightningElement {
-   @api recordId;
-   @api objectApiName;
-   fields = ['AccountId', 'Name', 'Title', 'Phone', 'Email'];
-}
