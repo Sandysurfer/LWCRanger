33cffbb67d2228b684f32003b6816e9da73fc08e
Sandysurfer <115895789+Sandysurfer@users.noreply.github.com>
2023-12-19 13:27:31 +0530

    newchangesmade

---
+++ force-app/main/default/lwc/customDataTypes/customDataTypes.js
@@ -0,0 +1,27 @@
+import LightningDataTable from "lightning/datatable";
+import customNameTemplate from "./customName.html";
+import customRankTemplate from "./customRank.html";
+import customImageTemplate from "./customImage.html";
+
+export default class CustomDataTypes extends LightningDataTable {
+
+    static customTypes = {
+        customNameType: {
+            template: customNameTemplate,
+            standardCellLayout: true,
+            typeAttributes: ["contactName"]
+        },
+        customRank: {
+            template: customRankTemplate,
+            standardCellLayout: false,
+            typeAttributes: ["rankIcon"]
+        },
+        customPicture: {
+            template: customImageTemplate,
+            standardCellLayout: true,
+            typeAttributes: ["pictureUrl"]
+        }
+    }
+
+
+}
